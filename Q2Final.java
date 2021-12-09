7484718afd07320e7641950f858436f2a4b723e5
William Jones <46946397+willjones11@users.noreply.github.com>
2021-12-09 11:09:49 -0500

    Create Q2Final.java

---
+++ Q2Final.java
@@ -0,0 +1,8 @@
+double answer = 1, d = 0.00001;// double variables are declared and defined
+while (answer*answer <= num){ // conditions are checked for the while loop
+answer += d; // a add statement 
+} // 
+if (answer*answer > num) { // condition is checked for the if statement
+answer -= d; // a subtraction statement 
+} // if loop ends
+// { answer = sqrt(num) }  the answer 
